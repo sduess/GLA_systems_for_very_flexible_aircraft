0eca98dbd60cf88c7fa88daf2b48118207cbfb5b
Stefanie Düssler <s.dussler20@imperial.ac.uk>
2026-04-18 17:02:43 +0200

    replace hardcoded paths with fileparts/fullfile and load settings from JSON
    Replace hardcoded paths with fileparts/fullfile. Load LQR tuning weights and design flags from a JSON case settings file.
    Generate design_name dynamically from modes_to_be_removed instead of hardcoding _woIP_wo5thBM.
    Extract wing_span and initial tip deflection as named constants.
    Add axis labels.

--- 04_Control_Design_Synthesis/example_run_to_get_LQG_controller.m
+++ 04_Control_Design_Synthesis/example_run_to_get_LQG_controller.m
@@ -1,45 +1,47 @@
-%% Example run to get an LQG controller
-% Just an example how to get the LQG controller from the linear model
-% 'Post-phd steffi' didn't have time yet to clean the work from 'very-close to
-% phd deadline steffi' :) Sorry if the code is not that clean.
+% Synthesises an LQG controller from a pre-processed linear state-space
+% system, then visualizes the performance with an open-loop and closed-loop 
+% gust response simulation and pole-zero map.
 
-%% Define directories and parameters
-% TODO: get rid of absolute path
-route_directory = 'C:\Users\User\Documents\GLA_systems_for_very_flexible_aircraft\04_Control_Design_Synthesis/';
-case_name ='superflexop_cruise_linear_FOM_clamped_IPdamped_wing_only_num_modes12';
+clear; clc;
 
-%% Set penalties for LQR tuning
-input_LQR_tuning = struct();
-input_LQR_tuning.idx_penalized_aero_state = []; 
-input_LQR_tuning.weights_for_penalized_aero_state =  []; 
-input_LQR_tuning.idx_penalized_modal_displacement =[1, 2]; 
-input_LQR_tuning.weights_for_penalized_modal_displacement = [140, 140];
-input_LQR_tuning.idx_penalized_modal_velocities =1:2; %
-input_LQR_tuning.weights_for_penalized_modal_velocities =[0.0, 0.0]
-input_LQR_tuning.idx_penalized_rbm_modes = []; 
-input_LQR_tuning.weights_for_penalized_rbm_modes = []; 
-input_LQR_tuning.initial_diagonal_values = 0; 
-input_LQR_tuning.weights_for_penalized_control_surfaces = 2e-4;
-input_LQR_tuning.R_values = 1; 
+%% Paths
+route_directory = fileparts(mfilename('fullpath'));
+addpath(fullfile(route_directory, '..', '05_Utils', 'matlab_functions'));
+simulink_file_directory = fullfile(route_directory, '..', '05_Utils', 'simulink_files');
 
-design_name = '';
-%% Create systems and control gains for LQG controller
-use_elevators=false;
-accelerator_sensors=false;
-sensors_only_z = true;
-only_pos = false;
-rotation_dot = false;
-join_cs = true;
-make_cs_symmetric = false;
-modes_to_be_removed = []; 
-design_name = strcat(design_name, '_acc', num2str(accelerator_sensors), ...
-    '_onlyz', num2str(sensors_only_z),...
-    '_onlypos', num2str(only_pos),...
-    '_rotdot', num2str(rotation_dot), ...
-    '_joinedcs', num2str(join_cs),...
-    '_make_cs_symmetric', num2str(make_cs_symmetric), ...
-    '_woIP_wo5thBM');
+%% SuperFLEXOP and cruise equilibrium conditions
+wing_span = 7.07;
+initial_normalized_wing_tip_deflection = 6.2;
+%% Load case settings from JSON
+case_name = 'superflexop_cruise_linear_FOM_clamped_IPdamped_wing_only_num_modes12';
+settings_file = fullfile(route_directory, 'case_settings', strcat('lqr_tuning_', case_name, '.json'));
+settings = jsondecode(fileread(settings_file));
 
+input_LQR_tuning    = settings.lqr_tuning;
+use_elevators       = settings.design_flags.use_elevators;
+accelerator_sensors = settings.design_flags.accelerator_sensors;
+sensors_only_z      = settings.design_flags.sensors_only_z;
+only_pos            = settings.design_flags.only_pos;
+rotation_dot        = settings.design_flags.rotation_dot;
+join_cs             = settings.design_flags.join_cs;
+make_cs_symmetric   = settings.design_flags.make_cs_symmetric;
+modes_to_be_removed = settings.design_flags.modes_to_be_removed;
+
+if isempty(modes_to_be_removed)
+    removed_modes_str = '';
+else
+    removed_modes_str = strcat('_wo_modes', sprintf('_%d', modes_to_be_removed));
+end
+
+design_name = strcat('_acc',               num2str(accelerator_sensors), ...
+                     '_onlyz',             num2str(sensors_only_z), ...
+                     '_onlypos',           num2str(only_pos), ...
+                     '_rotdot',            num2str(rotation_dot), ...
+                     '_joinedcs',          num2str(join_cs), ...
+                     '_make_cs_symmetric', num2str(make_cs_symmetric), ...
+                     removed_modes_str);
+
+%% Synthesise LQG controller
 setup_LQG_controller(route_directory, ...
                      case_name, ...
                      use_elevators, ...
@@ -53,60 +55,80 @@ setup_LQG_controller(route_directory, ...
                      make_cs_symmetric, ...
                      modes_to_be_removed);
 
-%% Test LQR controller on a discrete gust with H=10m and I=10%
+%% Load synthesised controller
 if use_elevators
     case_name = strcat(case_name, '_elev_on');
 end
 case_name = strcat(case_name, design_name);
-load(strcat('./controller_files_matlab/', case_name, '.mat'));
-% Define directory where to find simulink files
-addpath(strcat(route_directory,'/../05_Utils/matlab_functions/'));
-simulink_file_directory = strcat(route_directory, '/../05_Utils/simulink_files/');
+load(fullfile(route_directory, 'controller_files_matlab', strcat(case_name, '.mat')));
 
-% Get gust input
+%% Gust response validation (H=10 m, I=10%)
 gust_time_series = get_1minuscosine_gust_input(10, ...
                                                 0.1, ...
                                                 input_settings.dt, ...
                                                 input_settings.u_inf, ...
                                                 input_settings.flight_time);
 
-% Open-loop gust respons
+% Open-loop response
 simulation_output_open_loop = run_linear_closed_loop_LQR_response(sys_final, ...
-                    input_settings,...
+                    input_settings, ...
                     gust_time_series, ...
-                    controller_final.K.*0, ...
+                    controller_final.K .* 0, ...
                     simulink_file_directory);
 
+% Closed-loop LQR response
 simulation_output = run_linear_closed_loop_LQR_response(sys_final, ...
-                    input_settings,...
+                    input_settings, ...
                     gust_time_series, ...
                     controller_final.K, ...
                     simulink_file_directory);
 
-%% Plot
-num_cs = size(sys_LQR.B,2)-1
-figure(1); hold on;plot(simulation_output.actual_output.Time, simulation_output.actual_output.Data(:,input_settings.index.eta_tip([3 3+6]))./(7.07/2)*100+6.2); 
-plot(simulation_output_open_loop.actual_output.Time, simulation_output_open_loop.actual_output.Data(:,input_settings.index.eta_tip([3 3+6]))./(7.07/2)*100+6.2, 'k-'); hold off;
- figure(2); hold on;plot(simulation_output.control_input.Time, rad2deg(simulation_output.control_input.Data(:,1:num_cs)));hold off;
- figure(3); hold on;plot(simulation_output.control_input.Time, rad2deg(simulation_output.control_input.Data(:,num_cs+1:2*num_cs)));hold off;
-.Time, rad2deg(simulation_output.control_input.Data(:,1:num_cs/2)));hold off;
- figure(5); hold on;plot(simulation_output.control_input.Time, rad2deg(simulation_output.control_input.Data(:,num_cs+1:num_cs + num_cs/2)));hold off;
-figure(6); hold on;plot(simulation_output.actual_output.Time, simulation_output.actual_output.Data(:,input_settings.index.eta_tip([3 3+6]))./(7.07/2)*100+6.2); 
+%% Plot  (add new runs to existing figures to have a quick and dirty comparison of different tunings)
+num_cs = size(sys_LQR.B, 2) - 1;
+
+figure(1); hold on;
+plot(simulation_output.actual_output.Time, ...
+    simulation_output.actual_output.Data(:, input_settings.index.eta_tip([3 3+6])) ./ (wing_span/2) * 100 + initial_normalized_wing_tip_deflection);
+plot(simulation_output_open_loop.actual_output.Time, ...
+    simulation_output_open_loop.actual_output.Data(:, input_settings.index.eta_tip([3 3+6])) ./ (wing_span/2) * 100 + initial_normalized_wing_tip_deflection, 'k-');
+xlabel('time, s');
+ylabel('tip deflection, %')
+hold off;
+
+figure(2); hold on;
+plot(simulation_output.control_input.Time, ...
+    rad2deg(simulation_output.control_input.Data(:,1:num_cs)));
+xlabel('time, s'); 
+ylabel('aileron deflection rate, deg/s')
+hold off;
+
+figure(3); hold on;
+plot(simulation_output.control_input.Time, ...
+     rad2deg(simulation_output.control_input.Data(:, num_cs+1:2*num_cs)));
+xlabel('time, s'); ylabel('aileron deflection, deg')
+hold off;
 
 
-%% Get Poles
-num_cs = size(sys_LQR.B,2)-1
-system_open_loop = ss(sys_LQR.A(1:end-num_cs:1:end-num_cs,1:end-num_cs), sys_LQR.B(1:end-num_cs,:), sys_LQR.C(:,1:end-num_cs), sys_LQR.D, input_settings.dt);
-system_closed_loop = ss(sys_LQR.A-sys_LQR.B(:,1:num_cs) * controller_final.K, ...
-        sys_LQR.B(:,end),...
-        sys_LQR.C-sys_LQR.D(:,1:num_cs)*controller_final.K,...
-        sys_LQR.D(:,end), ...
-        input_settings.dt)
+figure(4); hold on;
+plot(simulation_output.control_input.Time, ...
+    rad2deg(simulation_output.control_input.Data(:, num_cs+1:num_cs+num_cs/2)));
+xlabel('time, s'); ylabel('left aileron deflection, deg')
+hold off;
 
-fig = figure(10)
-hold on;
+%% Pole-zero map
+system_open_loop = ss(sys_LQR.A(1:end-num_cs, 1:end-num_cs), ...
+                      sys_LQR.B(1:end-num_cs, :), ...
+                      sys_LQR.C(:, 1:end-num_cs), ...
+                      sys_LQR.D, ...
+                      input_settings.dt);
+system_closed_loop = ss(sys_LQR.A - sys_LQR.B(:, 1:num_cs) * controller_final.K, ...
+                        sys_LQR.B(:, end), ...
+                        sys_LQR.C - sys_LQR.D(:, 1:num_cs) * controller_final.K, ...
+                        sys_LQR.D(:, end), ...
+                        input_settings.dt);
 
-pzmap(system_open_loop, 'k')
-pzmap(system_closed_loop, 'r') 
-zgrid()
-
+figure(10); hold on;
+pzmap(system_open_loop, 'k');
+pzmap(system_closed_loop, 'r');
+zgrid();
+hold off;
