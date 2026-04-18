0744001ea13fbd9426fc12a93c6a873a75812fa9
Stefanie Düssler <s.dussler20@imperial.ac.uk>
2026-04-18 15:48:31 +0200

    refactor remove_inputs_from_state_space_model for clarity
    Replace the even/odd cell-array loop with four explicit labelled matrix operations. Rename gust_inputs_included to num_non_cs_inputs (it is a count, not a flag), num_inputs to num_cs_inputs, and list_removed_delta_state to list_removed_delta_states. Update docstring
    accordingly.

--- 05_Utils/matlab_functions/remove_inputs_from_state_space_model.m
+++ 05_Utils/matlab_functions/remove_inputs_from_state_space_model.m
@@ -1,29 +1,43 @@
-function sys_reduced = remove_inputs_from_state_space_model(sys, list_removed_inputs, gust_inputs_included)
-%remove_inputs_from_state_space_model Removes inputs from state-space model
-%   When removing control inputs (delta_dot) from the linear state-space 
-%   model, we have to not only remove the corresponding columns from B and
-%   D matrices but also the columns linked to the aileron deflection delta
-%   in the A and C as well as the corresponding rows in the A and B
-%   matrices. If the inputs contain the gust input, the number of inputs
-%   have to be corrected since the gust is not linked to delta in the state
-%   vector x.
+function sys_reduced = remove_inputs_from_state_space_model(sys, list_removed_inputs, num_non_cs_inputs)
+%remove_inputs_from_state_space_model Remove CS inputs from augmented system
+%   Removes control surface (CS) inputs from the state-space system. Since
+%   the system is augmented with delta positions as states, removing a CS
+%   input (delta_dot column) also requires removing:
+%       - the corresponding delta state column from A and C
+%       - the corresponding delta state row from A and B
+%   Trailing non-CS inputs (gust, thrust) have no associated delta state
+%   and must be excluded from the state index calculation.
+%
+%   Inputs:
+%       sys                - augmented state-space system (ss object)
+%       list_removed_inputs - 1-based indices of CS inputs to remove
+%       num_non_cs_inputs  - number of trailing non-CS inputs (e.g. 1 for
+%                            gust only, 2 for gust + thrust)
 
-num_inputs = size(sys.B,2) - gust_inputs_included * 1;
-num_states = size(sys.A,1);
-ss_matrices = {sys.A; sys.B; sys.C; sys.D};
-list_removed_delta_state =  num_states - num_inputs + list_removed_inputs;
-for i=1:size(ss_matrices,1)
-    if  ~mod(i,2) % even (B or D matrix)
-        removed_columns = list_removed_inputs;
-    else % uneven (A or C matrix)
-        removed_columns = list_removed_delta_state;
-    end
-    ss_matrices{i}(:, removed_columns) = [];
-        if i <= 2
-            % remove rows from A and B linked to state delta n+1
-            ss_matrices{i}(list_removed_delta_state, :) = [];
-        end
-end
-sys_reduced = ss(ss_matrices{1}, ss_matrices{2}, ss_matrices{3}, ss_matrices{4}, sys.Ts);
+    num_cs_inputs = size(sys.B, 2) - num_non_cs_inputs;
+    num_states    = size(sys.A, 1);
+
+    % Delta states occupy the last num_cs_inputs rows/columns of A.
+    % Map removed input indices to their corresponding delta state indices.
+    list_removed_delta_states = num_states - num_cs_inputs + list_removed_inputs;
+
+    A = sys.A;
+    B = sys.B;
+    C = sys.C;
+    D = sys.D;
+
+    % Remove delta state columns from A and C
+    A(:, list_removed_delta_states) = [];
+    C(:, list_removed_delta_states) = [];
+
+    % Remove delta state rows from A and B
+    A(list_removed_delta_states, :) = [];
+    B(list_removed_delta_states, :) = [];
+
+    % Remove delta_dot input columns from B and D
+    B(:, list_removed_inputs) = [];
+    D(:, list_removed_inputs) = [];
+
+    sys_reduced = ss(A, B, C, D, sys.Ts);
 
 end
